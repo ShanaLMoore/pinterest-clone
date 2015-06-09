ef999140af748ad0d371d25d8198214c9548a157
Joseph Goodwin <joe.goodwin@me.com>
2015-06-09 11:33:26 -0400

    added a slug to make url pretty

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,15 @@
 class PinsController < ApplicationController
-  
+
   def index
     @pins = Pin.all
   end
-  
+
   def show
     @pin = Pin.find(params[:id])
   end
-  
+
+  def show_by_name
+    @pin = Pin.find_by_slug(params[:slug])
+    render :show
+  end
 end
