7b73b0e4767de7988e9f4da7c3e832a74222afeb
Joseph Goodwin <joe.goodwin@me.com>
2015-06-11 10:19:46 -0400

    added tests

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -1,5 +1,19 @@
 require "spec_helper"
 
 RSpec.describe "Our Application Routes" do
-  
+  describe "Get /pins/name-:slug" do
+    
+    it 'renders the pins/show templates' do
+      pin = Pin.first
+      get "/pins/name-#{pin.slug}"
+      expect(response).to render_template("pins/show")
+    end
+
+    it 'populates the @pin variable with the appropriate pin' do
+      pin = Pin.first
+      get "/pins/name-#{pin.slug}"
+      expect(assigns[:pin]).to eq(pin)
+    end
+
+  end
 end
