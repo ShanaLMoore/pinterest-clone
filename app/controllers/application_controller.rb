c6bcc225340993aafb52e009b80970af3c6e64e8
Joseph Goodwin <joe.goodwin@me.com>
2015-06-26 13:58:36 -0400

    check if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def require_login
-    if current_user.nil?
+    if !logged_in?
       redirect_to :login
     end
   end
+
+  def logged_in?
+    !current_user.nil? && !current_user.id.nil?
+  end
+  helper_method :logged_in?
 
 end
