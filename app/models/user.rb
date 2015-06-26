daa310e48ab31e376254913fcf45b9d705ffa6ac
Joseph Goodwin <joe.goodwin@me.com>
2015-06-26 09:34:47 -0400

    destroying a user their pins will go too

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :pinnings
+  has_many :pinnings , dependent: :destroy
   has_many :pins, through: :pinnings
   validates_presence_of :first_name, :last_name, :email, :password
   validates_uniqueness_of :email
